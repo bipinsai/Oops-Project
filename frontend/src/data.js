a0128b7625d505a467fcb570e2eff3c3f67ad034
LAPTOP-ERINAG9G\sivar <f20171314@hyderabad.bits-pilani.ac.in>
2021-04-18 17:03:43 +0530

    MongooseDB

---
+++ frontend/src/data.js
@@ -0,0 +1,68 @@
+const data =
+{
+  products:
+  [
+      {
+        _id:'1',
+        name: 'Nike Slim Shirts',
+        category: 'Shirts',
+        image:'/images/p1.png',
+        price: 120,
+        countInStock: 10,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'High quality product',
+    },
+    {
+      _id:'2',
+      name: 'Nike Slim Shirts',
+      category: 'Shirts',
+      image:'/images/p1.png',
+      price: 140,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product',
+    },
+    {
+      _id:'3',
+      name: 'Nike Slim Shirts',
+      category: 'Shirts',
+      image:'/images/p1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product',
+    },
+    {
+      _id:'4',
+      name: 'Nike Slim Shirts',
+      category: 'Shirts',
+      image:'/images/p1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product',
+    },
+    {
+      _id:'5',
+      name: 'Nike Slim Shirts',
+      category: 'Shirts',
+      image:'/images/p1.png',
+      price: 200,
+      countInStock: 0,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product',
+    },
+  ],
+}
+
+export default data;
